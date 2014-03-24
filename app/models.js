775ff8a1205e60087c3689d53722d290db133380
Alan Hamilton <alan.b.hamilton@gmail.com>
2014-03-24 16:40:29 -0400

    initial model implementation

--- app/models.js
+++ app/models.js
@@ -2,16 +2,36 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
-var Contact = new Schema({
-    email:      { type: String },
-    name: {
-        first:  { type: String },
-        last:   { type: String }
-    },
-    phone:      { type: String },
-    gravatar:   { type: String }
+
+var User = new Schema({
+  name: {
+    first:   { type: String },
+    last:    { type: String }
+  },
+  email:     { type: String },
+  username:  { type: String },
+  password:  { type: String },
+  tags:      [ { type: String } ]
+});
+
+var Receipt = new Schema({
+  user:      {
+    type:    ObjectId,
+    ref:     'User'
+  },
+  title:     { type: String },
+  date:      { type: Date },
+  total:     { type: Number },
+  dateAdded: {
+    type:    Date,
+    default: Date.now
+  },
+  tags:      [ { type: String } ],
+  note:      { type: String },
+  fspath:    { type: String }
 });
 
 module.exports = {
-    Contact: mongoose.model('Contact', Contact)
+    User: mongoose.model('User', User),
+    Receipt: mongoose.model('Receipt', Receipt)
 };
