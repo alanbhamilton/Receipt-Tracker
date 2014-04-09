1be6bacfa01a4c9585afe5d593cbb9ed8c2fa1b8
Alan Hamilton <alan.b.hamilton@gmail.com>
2014-04-09 15:26:12 -0400

    now use mkdirp package to create user folders
    it creates folders recursively

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose'),
     async = require('async'),
     path = require('path'),
     fs = require('fs'),
+    mkdirp = require('mkdirp'),
     config = require('../config.json');
     // ObjectId = Schema.ObjectId;
 
@@ -22,18 +23,13 @@ var UserSchema = new Schema({
 
 UserSchema.methods.createReceiptFolders = function (cb) {
   var that = this;
-  var userFolder = path.join(config.user_data.folder, this._id.toString());
+  var folder = path.join(config.user_data.folder, this._id.toString(), '_in');
 
-  async.eachSeries([userFolder, userFolder + '/_in'],
-    function (folder, done) {
-      fs.mkdir(folder, done);
-    },
-    function (err) {
-      if (err) { return cb(err); }
-      console.log('created new user folder for: ' + that._id.toString());
-      return cb(null);
-    }
-  );
+  mkdirp(folder, function (err) {
+    if (err) { return cb(err); }
+    console.log('created new user folder for: ' + that._id.toString());
+    return cb(null);
+  });
 };
 
 // exports
