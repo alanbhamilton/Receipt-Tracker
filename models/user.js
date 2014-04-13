3736a74a887ae4c4ac4d3ee1712fa89c7eef2347
Alan Hamilton <alan.b.hamilton@gmail.com>
2014-04-13 14:33:08 -0400

    start of moving checking for new receipts to user.js

--- models/user.js
+++ models/user.js
@@ -23,6 +23,29 @@ UserSchema.methods.checkForNewReceipts = function (cb) {
   var id = this._id.toString();
   var folder = path.join(config.user_data.folder, id, '_in');
 
+  // if (procQueue) { return callback(null, null); }
+  fs.readdir(folder, function (err, newFiles) {
+    if (err) { cb(err, null); }
+    if (!newFiles) { callback(null, null); }
+    newFiles.forEach(function (fileName) {
+      // only look for files supported by leptonica
+      var regex = /^.+\.(jpg|jpeg|png|tiff|bmp|pnm|gif|ps|pdf|webp)$/;
+      var escaped;
+      if (regex.test(fileName)) {
+        escaped = fileName.replace(/(\s)/, "\\ ");  //Escape the spaces
+      }
+
+      var escaped = fileName.replace(/(\s)/, "\\ ");  //Escape the spaces
+      console.log('found new file: ' + fileName);
+      // procQueue += 1;
+      // ocr.receipt(path.join(imagesPath, escaped), function (err, receipt) {
+        // procQueue -= 1;
+        // if (err) { callback(err, null, null); }
+        // return callback(null, receipt, fileName);
+      // });
+    });
+  });
+
 };
 
 UserSchema.methods.createReceiptFolders = function (cb) {
@@ -31,7 +54,7 @@ UserSchema.methods.createReceiptFolders = function (cb) {
 
   mkdirp(folder, function (err) {
     if (err) { return cb(err); }
-    console.log('created new user folder for: ' + id;
+    console.log('created new user folder for: ' + id);
     return cb(null);
   });
 };
